0b5d7fa304cd7dac5c9dff01c13bf39e9e7a390f
richardmazimpk <richardmazimpk@gmail.com>
2026-03-17 08:58:38 +0200

    Add files via upload

---
+++ Generator Fuel Consumption.py
@@ -0,0 +1,9 @@
+fuel_per_hour=2.5
+price_per_liter=1800
+hours=5
+
+total_fuel=fuel_per_hour*hours
+total_cost=total_fuel*price_per_liter
+
+print("total fuel",total_fuel)
+print("total fuel cost",total_cost)
